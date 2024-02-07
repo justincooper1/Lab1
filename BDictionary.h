59135abef8790886d801780f333c680f451d0017
Justin <jccooper7@liberty.edu>
2024-02-07 16:33:53 -0500

    switched BDictionary to a smart pointer and trying to fix errors for compilation

--- BDictionary.h
+++ BDictionary.h
@@ -1,39 +1,49 @@
 #pragma once
 
 #include "dictionaryADT.h"
-#include "KVpair.h" // Assuming this is required for the dictionary implementation
+#include "KVpair.h"
+#include "ABag.h"
+#include <memory>
 
 template <class Key, class E>
 class BDictionary : public Dictionary<Key, E> {
 private:
-    ABag<KVpair<Key, E>> bag; // Use an array-based bag to store key-value pairs
+    std::unique_ptr<ABag<KVpair<Key, E>>> bag; // Smart pointer
 
 public:
     // Constructor
-    BDictionary() {}
+    BDictionary() {
+        bag = std::make_unique<ABag<KVpair<Key, E>>>(); // Initialize the pointer
+    }
+
+    // Constructor with initial size argument
+    BDictionary(size_t initialSize) {
+        bag = std::make_unique<ABag<KVpair<Key, E>>>(initialSize);
+    }
 
     // Destructor
-    ~BDictionary() {}
+    ~BDictionary() override {}
 
     // Reinitialize dictionary
     // Uses emptyBag from ABag.h
     void clear() override {
-        bag.emptyBag();
+        bag->emptyBag();
     }
 
     // Insert a record
     // Uses addItem from ABag.h
     bool insert(const Key& k, const E& e) override {
         KVpair<Key, E> newPair(k, e);
-        return bag.addItem(newPair);
+        return bag->addItem(newPair);
     }
 
     // Remove a record using key
     // Uses remove from ABag.h
-    bool remove(const Key& k, const E& rtnVal) override {
+    bool remove(const Key& k, E& rtnVal) override {
         KVpair<Key, E> temp;
         if (find(k, rtnVal)) {
-            bag.remove(temp);
+            bag->remove(temp);
+            rtnVal = temp.value();
             return true;
         }
         return false;
@@ -42,8 +52,9 @@ public:
     // Remove any record from the dictionary
     // Uses inspectTop and removeTop from ABag.h
     bool removeAny(E& returnValue) override {
-        if (bag.inspectTop(returnValue)) {
-            bag.removeTop(returnValue);
+        KVpair<Key, E> temp;
+        if (bag->removeTop(temp)) {
+            returnValue = temp.value();
             return true;
         }
         return false;
@@ -54,12 +65,16 @@ public:
     bool find(const Key& k, E& returnValue) const override {
         KVpair<Key, E> temp;
         temp.setKey(k);
-        return bag.search(temp);
+        if (bag->search(temp)) {
+            returnValue = temp.value();
+            return true;
+        }
+        return false;
     }
 
     // Return the size of the dictionary
     // Uses numitems from ABag.h
     int size() const override {
-        return bag.numItems();
+        return bag->numItems();
     }
 };
